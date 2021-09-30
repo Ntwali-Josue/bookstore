562fce4d4f75cf34accfea3dcf8b0a8afb345b4a
Ntwali-Josue <kntwalijos@gmail.com>
2021-09-30 21:43:52 +0200

    t

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  // Route,
+} from 'react-router-dom';
+// import Books from './components/Books';
+// import Categories from './components/Categories';
+import Navbar from './components/NavBar';
+// import FormComponent from './components/Form';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Empty project</h1>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Router>
+      <Navbar />
+      <div className="app">
+        <div className="app-container">
+          <Switch>
+            {/* <Route path="/categories">
+              <Categories />
+            </Route>
+            <Route exact path="/">
+              <Books />
+              <FormComponent />
+            </Route> */}
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  </>
+);
 
 export default App;
