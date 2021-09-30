5b0bbc027de1ca4ca9a24109e8b69d893b591781
Ntwali-Josue <kntwalijos@gmail.com>
2021-09-30 23:11:51 +0200

    Update import for the bookItem component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import BookItem from './BookItem';
+import BookItem from '../Redux/books/BookItem';
 
 const BookList = () => {
   const booksDetails = [
