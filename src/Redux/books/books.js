1cf517d0163a02813de4eab3b6efb0eb992368dd
Ntwali-Josue <kntwalijos@gmail.com>
2021-10-01 20:07:59 +0200

    Edit addBook function to fetch the api

--- src/Redux/books/books.js
+++ src/Redux/books/books.js
@@ -1,12 +1,26 @@
+import axios from 'axios';
+
+const GET_BOOKS = 'bookStore/books/GET_BOOKS';
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const fetchURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/KWzu5jmR1345eEuN2hZp/books';
 
 const initialState = [];
 
-export const addBook = (book) => ({
-  type: ADD_BOOK,
-  book,
-});
+export const addBook = (book) => async (dispatch) => {
+  const result = await axios.post(fetchURL, {
+    item_id: book.item_id,
+    title: book.title,
+    category: book.category,
+  });
+  const addedBook = await result.data;
+  if (addedBook === 'Created') {
+    dispatch({
+      type: ADD_BOOK,
+      book,
+    });
+  }
+};
 
 export const removeBook = (book) => ({
   type: REMOVE_BOOK,
